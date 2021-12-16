77211a6195fc7c2e4d176ca1d7ae69d401fe57cf
Vegard Stikbakke <vegard.stikbakke@gmail.com>
2021-12-16 12:03:08 +0100

    Use default Homebrew path on Macs
    See [this PR](https://github.com/alexbrainman/odbc/pull/168) on the repo this is forked from.

--- api/api_unix.go
+++ api/api_unix.go
@@ -7,7 +7,8 @@
 
 package api
 
-// #cgo darwin LDFLAGS: -lodbc
+// #cgo darwin LDFLAGS: -L /usr/local/opt/unixodbc/lib -lodbc
+// #cgo darwin CFLAGS: -I /usr/local/opt/unixodbc/include
 // #cgo linux LDFLAGS: -lodbc
 // #cgo freebsd LDFLAGS: -L /usr/local/lib -lodbc
 // #cgo freebsd CFLAGS: -I/usr/local/include
