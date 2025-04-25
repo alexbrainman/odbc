fd97e4030c7600750291df77b222c32b0093b48e
Ajaysinh Rathod <63580687+Ajaysinh1290@users.noreply.github.com>
2025-04-25 10:34:19 +0530

    Fix: Byte Parameter Type in Redshift (#211)
    * Fix: Avoid direct access to first element in byte slice to prevent panic
    
    * Avoid changing other values
    
    * redshift bug fix for error "type conversion is not supported from 12 SQL type to -2 C type"
    
    * formatting file param.go
    
    ---------
    
    Co-authored-by: Ajaysinh Rathod <Ajaysinh.Rathod1@ibm.com>

--- param.go
+++ param.go
@@ -144,22 +144,11 @@ func (p *Parameter) BindValue(h api.SQLHSTMT, idx int, v driver.Value, conn *Con
 		p.Data = b
 		if len(d) > 0 {
 			buf = unsafe.Pointer(&b[0])
-		} else {
-			buf = nil
 		}
 		buflen = api.SQLLEN(len(b))
 		plen = p.StoreStrLen_or_IndPtr(buflen)
 		size = api.SQLULEN(len(b))
-		switch {
-		case p.isDescribed:
-			sqltype = p.SQLType
-		case size <= 0:
-			sqltype = api.SQL_LONGVARBINARY
-		case size >= 8000:
-			sqltype = api.SQL_LONGVARBINARY
-		default:
-			sqltype = api.SQL_BINARY
-		}
+		sqltype = api.SQL_LONGVARBINARY
 	default:
 		return fmt.Errorf("unsupported type %T", v)
 	}
